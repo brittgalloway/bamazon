1b8a4fc82b3c4388d69aeb822138bcd6a540edb2
BrittGall <crlnfllr@gmail.com>
2019-10-14 01:09:32 -0500

    mysql connected

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,12 +1,15 @@
-const mysql = reqiure("mysql");
+const mysql = require("mysql");
 const inquirer = require("inquirer");
+
 require("dotenv").config();
 
-const connection = mysql.createConnection({});
-mysql.connect({
+const connection = mysql.createConnection({
   host: process.env.host,
   port: process.env.port,
   username: process.env.username,
   password: process.env.password,
   database: process.env.database
+});
+connection.connect(function(err) {
+  console.log("Connected as id: " + connection.threadId);
 });
